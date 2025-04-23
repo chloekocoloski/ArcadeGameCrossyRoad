91ddefde1e528c373023078dcc2d9f512a00ad36
chloekocoloski <chloe_kocoloski27@milton.edu>
2025-04-22 21:18:40 -0400

    Initial commit

---
+++ src/Astronaut.java
@@ -0,0 +1,155 @@
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ */
+public class Astronaut {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;            //a boolean to denote if the hero is alive or dead.
+    public Rectangle rec;
+    public boolean isCrashing;
+    public boolean up;
+    public boolean down;
+    public boolean right;
+    public boolean left;
+
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public Astronaut(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx = 8; //direction and speed, left and right
+        dy = 8; //slope
+        width = 100;
+        height = 100;
+        isAlive = true;
+        rec = new Rectangle(xpos, ypos, width, height);
+        isCrashing = false;
+        up = false;
+        down = false;
+        right = false;
+        left = false;
+
+
+    } // constructor
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec = new Rectangle(xpos, ypos, width, height);
+
+
+    }
+
+    public void bounce() {
+        //moving up
+        if (up == true) {
+            dy = -5;
+        }
+        //moving down
+        if (down == true) {
+            dy = 5;
+        }
+        //moving left
+        if (left == true) {
+            dx = -5;
+        }
+        //moving right
+        if (right == true) {
+            dx = 5;
+        }
+        if (up == false && down == false) {
+            dy = 0;
+        }
+
+        if (left == false && right == false) {
+            dx = 0;
+        }
+        //bounce off the east wall
+        if (xpos > 950) {
+            dx = -dx;
+        }
+
+        //bounce of the West Wall
+        if (xpos < 0) {
+            dx = -dx;
+        }
+        //bounce of the South Wall
+        if (ypos > 650) {
+            dy = -dy;
+
+        }
+
+        //bounce of the North Wall
+        if (ypos < 0) {
+            dy = -dy;
+        }
+
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec = new Rectangle(xpos, ypos, width, height);
+
+
+
+    }
+
+    public void wrap() {
+        //hits the east wall
+
+        if (xpos > 950) {
+            xpos = 0;
+        }
+
+        //hits the west wall
+
+        if (xpos < 0) {
+            xpos = 950;
+        }
+
+        //hits the south wall
+        if (ypos > 650) {
+            ypos = 0;
+        }
+
+        //hits the north wall
+        if (ypos < 0) {
+            ypos = 650;
+        }
+
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        rec = new Rectangle(xpos, ypos, width, height);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/CrossyRoad.java
@@ -0,0 +1,216 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+
+
+    public class CrossyRoad implements Runnable, KeyListener {
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println(e.getKeyChar());
+            System.out.println(e.getKeyCode());
+
+            //moving up
+            if(e.getKeyCode() == 38){
+                System.out.println("up");
+                astro.up = true;
+                astro.down = false;
+            }
+            //moving down
+            if(e.getKeyCode() == 40){
+                System.out.println("down");
+                astro.down = true;
+                astro.up = false;
+            }
+            //moving left
+            if(e.getKeyCode() == 37){
+                System.out.println("left");
+                astro.left = true;
+                astro.right = false;
+            }
+            //moving right
+            if(e.getKeyCode() == 39){
+                System.out.println("right");
+                astro.right = true;
+                astro.left = false;
+            }
+        }
+
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("released?");
+            System.out.println(e.getKeyChar());
+            //moving up
+            if(e.getKeyCode() == 38){
+                astro.up = false;
+            }
+            //moving down
+            if(e.getKeyCode() == 40){
+                astro.down = false;
+            }
+            //moving left
+            if(e.getKeyCode() == 37){
+                astro.left = false;
+            }
+            //moving right
+            if(e.getKeyCode() == 39){
+                astro.right = false;
+            }
+        }
+
+        final int WIDTH = 1000;
+        final int HEIGHT = 700;
+
+        public JFrame frame;
+        public Canvas canvas;
+        public JPanel panel;
+
+        public BufferStrategy bufferStrategy;
+        public Image astroPic;
+        public Image astro2Pic;
+        public Image backgroundPic;
+
+        private Astronaut astro;
+        private Astronaut astro2;
+
+        Astronaut [] astronautsArray = new Astronaut[10];
+
+
+
+        public static void main(String[] args) {
+            CrossyRoad ex = new CrossyRoad();
+            new Thread(ex).start();
+        }
+
+        public CrossyRoad() {
+
+            setUpGraphics();
+
+
+            astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png");
+            astro2Pic = Toolkit.getDefaultToolkit().getImage("astronaut2.png");
+            backgroundPic = Toolkit.getDefaultToolkit().getImage("no picture rn");
+
+            astro = new Astronaut(200,500);
+            astro2 = new Astronaut(500,600);
+
+            for(int x = 0; x <astronautsArray.length; x++) {
+                astronautsArray[x] = new Astronaut((int) (Math.random()* 900), (int)(Math.random()* 600));
+            }
+        }
+
+        public void run() {
+            //for the moment we will loop things forever.
+            while (true) {
+                moveThings();  //move all the game objects
+                render();  // paint the graphics
+                pause(20); // sleep for 10 ms
+            }
+        }
+
+
+        public void moveThings() {
+            collisions();
+            astro.bounce();
+            astro2.wrap();
+
+            for(int y = 0; y < astronautsArray.length; y++){
+                astronautsArray[y].wrap();
+            }
+        }
+
+        public void collisions(){
+            if(astro.rec.intersects(astro2.rec) && astro.isCrashing == false && astro.isAlive && astro2.isAlive){
+                System.out.println("explosion!!!!!!!");
+                astro.dx = -astro.dx;
+                astro.dy = -astro.dy;
+                astro2.dx = -astro.dx;
+                astro2.dy = -astro2.dy;
+                astro2.isAlive = false;
+                astro.width = astro.width + 30;
+                astro.height = astro.height + 30;
+                astro2.dx = astro2.dx + 30;
+                astro2.dy = astro2.dy + 30;
+                astro.isCrashing = true;
+            }
+
+            if(!astro.rec.intersects(astro2.rec)){
+                astro.isCrashing = false;
+            }
+
+            for(int b = 0; b < astronautsArray.length; b++){
+                if(astro.rec.intersects(astronautsArray[b].rec)){
+                    System.out.println("crashing");
+                }
+            }
+
+
+
+        }
+
+        public void pause(int time ){
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void setUpGraphics() {
+            frame = new JFrame("Application Template");
+
+            panel = (JPanel) frame.getContentPane();
+            panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+            panel.setLayout(null);
+
+
+            canvas = new Canvas();
+            canvas.setBounds(0, 0, WIDTH, HEIGHT);
+            canvas.setIgnoreRepaint(true);
+
+            canvas.addKeyListener(this);
+            panel.add(canvas);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setResizable(false);
+            frame.setVisible(true);
+
+            canvas.createBufferStrategy(2);
+            bufferStrategy = canvas.getBufferStrategy();
+            canvas.requestFocus();
+            System.out.println("DONE graphic setup");
+
+        }
+
+
+        private void render() {
+            Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+            g.clearRect(0, 0, WIDTH, HEIGHT);
+
+            g.drawImage(backgroundPic, 0, 0, WIDTH, HEIGHT, null);
+
+            g.drawImage(astroPic, astro.xpos, astro.ypos, astro.width, astro.height, null);
+
+            if(astro2.isAlive == true) {
+                g.drawImage(astro2Pic, astro2.xpos, astro2.ypos, astro2.width, astro2.height, null);
+            }
+
+            for(int l = 0; l < astronautsArray.length; l++){
+                g.drawImage(astroPic, astronautsArray[l].xpos, astro.ypos, astro.width, astro.height, null);
+
+            }
+
+
+            g.dispose();
+
+            bufferStrategy.show();
+        }
+
+    }
+
